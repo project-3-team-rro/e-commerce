88f8c7ecf371ef4295cd6f5d29214ee762581df4
olaborde <oml007@live.com>
2018-05-19 09:33:48 -0400

    added backend for login signup and logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// tslint:disable-next-line:import-blacklist
+// import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
+import {AuthService} from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private myService: AuthService){}
+
+  formInfo: any = {username: '', password: ''};
+
+  user: any;
+
+  error: any;
+
   title = 'app';
+
+
+  login() {
+    this.myService.login(this.formInfo)
+      .subscribe(
+        (user) => this.user = user,
+        (err) => this.error = err
+      );
+  }
+
+  signup() {
+    this.myService.signup(this.formInfo)
+      .subscribe(
+        (user) => {this.user = user;
+          console.log(this.user);
+        },
+        (err) => this.error = err
+      );
+  }
+
+
+  logout() {
+    this.myService.logout()
+    .subscribe(
+    () => {this.user = null;
+      this.formInfo = {};
+    },
+    (err) => this.error = err
+  );
+  }
+
+
 }
