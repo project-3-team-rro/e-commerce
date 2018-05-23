d42ced88c02c39aef6e7e5382d35954b7648f28c
Richard Batista <stalfos@icloud.com>
2018-05-23 17:14:40 -0400

    Further work on user profile update form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-<<<<<<< HEAD
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -12,55 +11,30 @@ import { SearchComponent } from './search/search.component';
 import { MerchandiseListComponent } from './merchandise-list/merchandise-list.component';
 import { MerchandiseDetailsComponent } from './merchandise-details/merchandise-details.component';
 import { EditComponent } from './edit/edit.component';
-
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'index', pathMatch: 'full' },
   { path: 'merchandise', component: MerchandiseListComponent },
   { path: 'merchandise/:id', component: MerchandiseDetailsComponent },
+  { path: 'profile', component: UserProfileComponent }
 ];
-=======
-import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { AuthService } from "./services/auth.service";
-import { UserProfileComponent } from './user-profile/user-profile.component';
-
-const routes : Routes = [
-  {
-    path: '',
-    component: AppComponent
-  },
-  {
-    path: 'user-profile',
-    component: UserProfileComponent
-  }
-]
->>>>>>> Working on user profile
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     SearchComponent,
     MerchandiseListComponent,
     MerchandiseDetailsComponent,
     EditComponent,
+    UserProfileComponent
   ],
   imports: [
     HttpModule,
     BrowserModule,
     FormsModule,
     RouterModule.forRoot(routes),
-=======
-    UserProfileComponent
-  ],
-  imports: [
-  HttpModule,
-  BrowserModule,
-  FormsModule,
-  RouterModule.forRoot(routes)
->>>>>>> Working on user profile
   ],
   providers: [MerchandiseService, AuthService],
   bootstrap: [AppComponent]
